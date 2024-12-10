bb2c329b9b71e0c809d5d9b403330efbe67e5047
Javier Ornelas <javier.ornelasv@gmail.com>
2024-12-09 18:16:44 -0600

    feat: add validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :name, presence: :true
+  validates :email, presence: :true
+  validates :username, presence: :true
+  validates :password_confirmation, presence: true
+
+  has_secure_password
 end
